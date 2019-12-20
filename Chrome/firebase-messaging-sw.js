961965fed230d7ca3eac9ee5a7109dc58c1996e4
SOOHYUNLIM <56705480+SOOHYUNLIM@users.noreply.github.com>
2019-12-20 11:08:00 +0900

    Update firebase-messaging-sw.js

--- Chrome/firebase-messaging-sw.js
+++ Chrome/firebase-messaging-sw.js
@@ -4,14 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/7.5.2/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.5.2/firebase-messaging.js')
 
 var config = {
-  apiKey: "AIzaSyB5h1dtyUXyYroO4ZengUlSKCa93-WoRnU",
-  authDomain: "jarvis-77f82.firebaseapp.com",
-  databaseURL: "https://jarvis-77f82.firebaseio.com",
-  projectId: "jarvis-77f82",
-  storageBucket: "jarvis-77f82.appspot.com",
-  messagingSenderId: "172501072688",
-  appId: "1:172501072688:web:e1d67ee6f83f43711ba6f5",
-  measurementId: "G-GRFQ9P114V"
+ 
 };
 firebase.initializeApp(config);
 
